43cf49b033bf6507b6873f567f5aa55422b19d2a
luv srivastava <srivastavalove9452442@gmail.com>
2025-10-13 03:18:31 +0530

    Add files via upload

---
+++ day28.c
@@ -0,0 +1,41 @@
+//question1
+#include <stdio.h>
+int main(){
+/*Write a program to print all the prime numbers from 1 to n.
+Input 1:
+10
+Output 1:
+2 3 5 7
+Input 2:
+20
+Output 2:
+2 3 5 7 11 13 17 19*/
+int n, i, j, isPrime;
+scanf("%d", &n);
+for(i = 2; i <= n; i++) {
+isPrime = 1;
+for(j = 2; j * j <= i; j++) {
+if(i % j == 0) {
+isPrime = 0;
+break; }
+}
+if(isPrime) {
+printf("%d ", i); }
+}
+return 0; }
+//question2
+#include <stdio.h>
+int main(){
+/*Read and print elements of a one-dimensional array.
+Input 1:
+3
+10 20 30
+Output 1:
+10 20 30
+Input 2:
+5
+1 2 3 4 5
+Output 2:
+1 2 3 4 5*/
+    return 0;
+}
---
+++ day29.c
@@ -0,0 +1,32 @@
+//question1
+#include <stdio.h>
+int main(){
+/*Find the sum of array elements.
+Input 1:
+4
+2 4 6 8
+Output 1:
+20
+Input 2:
+3
+1 1 1
+Output 2:
+3*/
+    return 0;
+}
+//question2
+#include <stdio.h>
+int main(){
+/*Find the maximum and minimum element in an array.
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10*/
+    return 0;
+}
