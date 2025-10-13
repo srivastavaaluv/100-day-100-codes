0e7e1c0e85561e58fbf6d0983a118f305e2cf75c
luv srivastava <srivastavalove9452442@gmail.com>
2025-10-13 17:55:51 +0530

    Update day29.c

--- day29.c
+++ day29.c
@@ -12,6 +12,13 @@ Input 2:
 1 1 1
 Output 2:
 3*/
+    int n, i, sum = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i]; }
+    printf("%d", sum);
     return 0;
 }
 //question2
@@ -28,5 +35,22 @@ Input 2:
 10 10 10
 Output 2:
 Max=10, Min=10*/
+    int n;
+    scanf("%d", &n);        // Read number of elements
+    int arr[n];
+    // Read array elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0], min = arr[0];
+ // Find max and min
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+    // Print results
+    printf("Max=%d, Min=%d\n", max, min);
     return 0;
 }
